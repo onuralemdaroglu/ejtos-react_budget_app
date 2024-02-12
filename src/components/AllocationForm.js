428ae6a7eaf6e486303c56160b67b07b48380584
Onur Alemdaroglu <kronosscall@hotmail.com>
2024-02-12 16:57:27 -0500

    updated files

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -58,7 +58,7 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
-
+                    <span style={{ marginLeft: '2rem', size: 10 }}>£</span>
                     <input
                         required='required'
                         type='number'
@@ -67,6 +67,8 @@ const AllocationForm = (props) => {
                         style={{ marginLeft: '2rem' , size: 10}}
                         onChange={(event) => setCost(event.target.value)}>
                         </input>
+
+                    
 
                     <button className="btn btn-primary" onClick={submitEvent} style={{ marginLeft: '2rem' }}>
                         Save
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,15 +2,15 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, currency } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
         setNewBudget(event.target.value);
     }
     return (
 <div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+<span>Budget: {currency}</span>
+<input type="number" step="10" max={20000} value={newBudget} onChange={handleBudgetChange}></input>
 </div>
     );
 };
