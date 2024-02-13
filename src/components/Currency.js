4f1ff9f4a65ffdff8e347754cbcfb2887e59b9f2
Onur Alemdaroglu <kronosscall@hotmail.com>
2024-02-12 22:35:03 -0500

    currency_dropdown updated

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -13,10 +13,10 @@ const Currency = () => {
 
     return (
         <div className='alert alert-secondary'> {Currency.value}<span>Currency</span> {
-            <select name='Currency' id="Currency" onChange={event=>changeCurrency(event.target.value)}>
-                <option value='$'>€ Euro</option>
+            <select name='Currency' id="Currency" style={{backgroundColor: 'lightgreen', borderRadius: '5px'}} onChange={event=>changeCurrency(event.target.value)}>
+                <option value='€'>€ Euro</option>
                 <option value='£'>£ Pound</option>
-                <option value='€'>$ Dollar</option>
+                <option value='$'>$ Dollar</option>
                 <option value='₹'>₹ Ruppee</option>
             </select>
         }
